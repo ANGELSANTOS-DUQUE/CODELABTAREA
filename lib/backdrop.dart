5f779c47611bab85b8c2af0205e696f589b095c2
ANGELSANTOS-DUQUE <0501200902075@iteesa.edu.hn>
2026-03-15 10:58:19 -0600

    Commit43

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -162,7 +162,7 @@ class _BackdropState extends State<Backdrop>
     return Scaffold(
       appBar: appBar,
       // TODO: Return a LayoutBuilder widget (104)
-      body: _buildStack(),
+      body: LayoutBuilder(builder: _buildStack),
     );
   }
 }
