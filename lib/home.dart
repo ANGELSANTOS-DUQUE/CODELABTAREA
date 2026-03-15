5d1c1ef6a638e6239d200bbfa2cbec0e2e4c0e6c
ANGELSANTOS-DUQUE <0501200902075@iteesa.edu.hn>
2026-03-15 09:01:18 -0600

    Commit22

--- lib/home.dart
+++ lib/home.dart
@@ -62,14 +62,17 @@ class HomePage extends StatelessWidget {
                     // TODO: Handle overflowing labels (103)
                     Text(
                       product.name,
-                      style: theme.textTheme.titleLarge,
+                      style: theme.textTheme.labelLarge,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
                       maxLines: 1,
                     ),
-                    const SizedBox(height: 8.0),
+                    const SizedBox(height: 4.0),
                     Text(
                       formatter.format(product.price),
-                      style: theme.textTheme.titleSmall,
+                      style: theme.textTheme.bodySmall,
                     ),
+                    // End new code
                   ],
                 ),
               ),
